70979eb03f55290fdaf1e8e58a63c6b2eed36d74
kuyaguard <renzoHernandez01@gmail.com>
2025-12-15 15:50:28 +0800

    update
    1

--- src/app/providers.js
+++ src/app/providers.js
@@ -19,6 +19,7 @@ export default function Providers({ children }) {
             <CartProvider>
               <WishListProvider>
                 <NotFoundProvider>
+                  <MSWProvider/>
                 {children}
                 <div id="overlay-root"></div>
                 </NotFoundProvider>
